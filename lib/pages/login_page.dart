50b20c9ec0d4a6191116074c8860e61d4fb02f92
Abykirwa1937 <Abigaelkirwa1937@gmail.com>
2023-04-20 10:02:28 +0300

    first commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body:Column(children:[
+        //logo
+
+        //welcome back
+
+        //username text
+
+        //password text
+
+        //forgo password 
+
+        //sign in button
+      ]
+      ),
+    );
+  }
+}
